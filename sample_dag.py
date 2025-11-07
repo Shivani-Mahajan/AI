6ad32dade92f851c7dc0c24d7eb56d07124eab99
Shivani-Mahajan <97105148+Shivani-Mahajan@users.noreply.github.com>
2025-11-07 17:33:57 +0530

    Update sample_dag.py

--- sample_dag.py
+++ sample_dag.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
+#from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
@@ -13,8 +13,8 @@ with DAG(
     dag_id='our_first_dag',
     default_args=default_args,
     description='This is our first dag',
-    start_date= days_ago(2),
-    schedule_interval=None,
+    start_date= datetime(2022, 1, 1),
+    schedule=None,
     #start_date=datetime(2023, 9, 12, 2),
     #schedule_interval='@daily'
     access_control={
